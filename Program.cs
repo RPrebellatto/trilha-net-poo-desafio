f9cc0608a326223ee8c81d3a35cd16541e9c8c08
RPRebellatto <rogerio.rebellatto@gmail.com>
2023-05-08 12:12:06 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado
+
+Console.WriteLine("Novo Nokia");
+
+Smartphone nokia = new Nokia("1234124", "M1", "112343", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Novo IPhone");
+Smartphone iPhone = new IPhone("23455542", "11", "645632", 128);
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Instagram");
